3fd0334d499b44fd6b23bf3e521910e98dda84d9
Leon Miller-Out <leon@singlebrook.com>
2013-10-22 13:27:49 -0400

    Add icon font

--- lib/sb_internal_style.rb
+++ lib/sb_internal_style.rb
@@ -2,5 +2,9 @@ require "sb_internal_style/version"
 
 module SbInternalStyle
   class Engine < ::Rails::Engine
+    config.after_initialize do
+      # Configure hosting application to precompile font files
+      Rails.application.config.assets.precompile += %w( .svg .eot .woff .ttf )
+    end
   end
 end
